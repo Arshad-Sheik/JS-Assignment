509b3732d901c23abd6694afb2c5f6d2bd791799
Arshad-Sheik <68222195+Arshad-Sheik@users.noreply.github.com>
2020-07-23 22:07:47 +0530

    Delete Question2.js

--- Day 3-ASSIGNMENTS/Question2.js
+++
@@ -1,8 +0,0 @@
-    var response = prompt("Enter your OS name and its version seperated by space");
-    var osversion = response.split(" ");
-    console.log(osversion);
-    var os = osversion.slice(0,1);
-    os = String(os);
-    var version = osversion.slice(1);
-    version = Number(version);
-    console.log(`The OS name is ${os} and version is ${version}`)
